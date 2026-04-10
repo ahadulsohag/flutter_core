c083a3301c6a1f1cfd0d69c78590cd0eb1ba4512
Sohag <ahadulsohag@gmail.com>
2026-04-10 22:16:13 +0600

    abc

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo'),
     );
   }
 }
@@ -35,9 +35,30 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 29, 29, 89),
-        centerTitle: true,
-        title: Text(widget.title, style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.amber,
+        title: Text(
+          'Flutter Demo',
+          
+          style: TextStyle(color: Colors.black, ),
+        ),
+      ),
+
+      body: Center(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          margin: EdgeInsets.all(50.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24),
+            color: Colors.blue,
+          ),
+          child: Center(
+            child: Text(
+              'data',
+              style: TextStyle(color: const Color.fromARGB(255, 243, 243, 14)),
+            ),
+          ),
+        ),
       ),
     );
   }
