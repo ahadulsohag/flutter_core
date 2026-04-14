c41b615f8e13f38ad12064279c60a99eea10052a
Sohag <ahadulsohag@gmail.com>
2026-04-15 00:08:17 +0600

    abc

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -14,66 +14,77 @@ class _HomePageState extends State<HomePage> {
   double sliderValue = 0.0;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsetsGeometry.all(20.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: controller,
-            decoration: InputDecoration(border: OutlineInputBorder()),
-            onEditingComplete: () {
-              setState(() {});
-            },
-          ),
-          Text(controller.text),
-          Checkbox(
-            tristate: true,
-            value: isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-          ),
-          CheckboxListTile(
-            value: isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-            title: Text('Click me'),
-            tristate: true,
-          ),
-          Switch(
-            value: isSwitched,
-            onChanged: (bool value) {
-              setState(() {
-                isSwitched = value;
-              });
-            },
-          ),
-          SwitchListTile.adaptive(
-            value: isSwitched,
-            title: Text('Switch me'),
-            onChanged: (bool value) {
-              setState(() {
-                isSwitched = value;
-              });
-            },
-          ),
-          Slider(
-            value: sliderValue,
-            max: 100.0,
-            divisions: 10,
-            onChanged: (double value) {
-              setState(() {
-                sliderValue = value;
-              });
-              print(sliderValue);
-            },
-          ),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsetsGeometry.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+            Text(controller.text),
+            Checkbox(
+              tristate: true,
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            CheckboxListTile(
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+              title: Text('Click me'),
+              tristate: true,
+            ),
+            Switch(
+              value: isSwitched,
+              onChanged: (bool value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            SwitchListTile.adaptive(
+              value: isSwitched,
+              title: Text('Switch me'),
+              onChanged: (bool value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            Slider(
+              value: sliderValue,
+              max: 100.0,
+              divisions: 10,
+              onChanged: (double value) {
+                setState(() {
+                  sliderValue = value;
+                });
+              },
+            ),
+            GestureDetector(
+              child: Image.asset('assets/images/pizza.jpg'),
+              onTap: () {
+                print('Selected the image!');
+              },
+            ),
+            Image.asset('assets/images/nature.jpeg'),
+            Image.asset('assets/images/nature.jpeg'),
+            Image.asset('assets/images/nature.jpeg'),
+            Image.asset('assets/images/nature.jpeg'),
+          ],
+        ),
       ),
     );
   }
