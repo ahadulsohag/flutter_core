a116a1ceb2b9a96fe924fe37f66e9e21b7ffe8eb
Sohag <ahadulsohag@gmail.com>
2026-05-08 01:25:56 +0600

    connecting API

---
+++ lib/classes/activity_classes.dart
@@ -0,0 +1,67 @@
+class Activity {
+  final String activity;
+  final double availability;
+  final String type;
+  final int participants;
+  final double price;
+  final String accessibility;
+  final String duration;
+  final bool kidFriendly;
+  final Uri? link;
+  final String key;
+
+  const Activity({
+    required this.activity,
+    required this.availability,
+    required this.type,
+    required this.participants,
+    required this.price,
+    required this.accessibility,
+    required this.duration,
+    required this.kidFriendly,
+    required this.link,
+    required this.key,
+  });
+
+  factory Activity.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'activity': String activity,
+        'availability': num availability,
+        'type': String type,
+        'participants': int participants,
+        'price': num price,
+        'accessibility': String accessibility,
+        'duration': String duration,
+        'kidFriendly': bool kidFriendly,
+        'link': String? link,
+        'key': String key
+      } => Activity(
+        activity: activity,
+        availability: (availability).toDouble(),
+        type: type,
+        participants: participants,
+        price: (price).toDouble(),
+        accessibility: accessibility,
+        duration: duration,
+        kidFriendly: kidFriendly,
+        link: link != null ? Uri.tryParse(link) : null,
+        key: key,
+      ),
+      _ => throw const FormatException('Failed to load activity.'),
+    };
+  }
+
+  Map<String, dynamic> toJson() => {
+        'activity': activity,
+        'availability': availability,
+        'type': type,
+        'participants': participants,
+        'price': price,
+        'accessibility': accessibility,
+        'duration': duration,
+        'kidFriendly': kidFriendly,
+        'link': link?.toString(),
+        'key': key,
+      };
+}
--- lib/views/pages/settings_page.dart
+++ lib/views/pages/settings_page.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_core/widgets/hero_widget.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_core/classes/activity_classes.dart';
 
 class SettingsPage extends StatefulWidget {
-  const SettingsPage(
-    {super.key,
-    required this.title,
-    }
-    );
+  const SettingsPage({super.key, required this.title});
   final String title;
   @override
   State<SettingsPage> createState() => _SettingsPageState();
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  Future getData() async {
+    var url = Uri.https('bored-api.appbrewery.com', '/random');
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return Activity.fromJson(
+        jsonDecode(response.body) as Map<String, dynamic>,
+      );
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +49,30 @@ class _SettingsPageState extends State<SettingsPage> {
           },
         ),
       ),
-      body: Text('data'),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+          if (snapshot.hasData) {
+            Activity activity = snapshot.data;
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    HeroWidget(title: 'HELL YEAH'),
+                    Text(activity.activity),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return Center(child: Text('Error'));
+          }
+        },
+      ),
     );
   }
 }
